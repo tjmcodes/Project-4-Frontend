c6b880056dd1398be01af22009effe7ab9f69d62
Teresa Morini <tjmcodes@gmail.com>
2022-08-11 01:05:51 +0100

    navbar item code started

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,34 +1,34 @@
-// import React from 'react'
+import React from 'react'
 
-// // import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 
-// const NavBar = () => {
-//   return <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
-//     <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Should be avatar image or hamburger menu</button>
+const NavBar = () => {
+  return <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
+    <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Should be avatar image or hamburger menu</button>
 
-//     <div id="dropdown" className="z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700">
-//       <ul className="py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefault">
-//         <li>
-//           <a href="/artist-login" className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Login</a>
-//         </li>
-//         <li>
-//           <a href="artists" className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">View artists</a>
-//         </li>
-//         <li>
-//           <a href="venues" className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">View venues</a>
-//         </li>
-//         <li>
-//           <a href="/artist-dashboard" className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">My dashboard</a>
-//         </li>
-//       </ul>
-//     </div>
-//   </nav>
+    <div id="dropdown" className="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700">
+      <ul className="py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefault">
+        <li>
+          <a href="/artist-login" className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Login</a>
+        </li>
+        <li>
+          <a href="artists" className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">View artists</a>
+        </li>
+        <li>
+          <a href="venues" className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">View venues</a>
+        </li>
+        <li>
+          <a href="/artist-dashboard" className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">My dashboard</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
   
   
-// }
+}
 
 
-// export default NavBar
+export default NavBar
 
 
 
